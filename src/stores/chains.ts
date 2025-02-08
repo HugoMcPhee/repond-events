0044903c3c19e98adcab94d5cd74ff67cc2cfdc3
Hugo McPhee <hugo.mcphee.public@gmail.com>
2025-02-08 18:51:37 +1100

    new item definitions

--- src/stores/chains.ts
+++ src/stores/chains.ts
@@ -1,10 +1,9 @@
-import { InitialItemsState } from "repond";
 import { EventBlock } from "../types";
 
 // Chains for running events in order
 
-function getDefaultState() {
-  return {
+export const chainsStore = {
+  newState: () => ({
     id: "", // chainId, this name is replaced when addign a chain
     liveEventIds: [] as string[], // the chain of event nodettes , ["showSpeech_abcd", "wait_abcd"]. when it's empty, the chain'sRemoved
     //
@@ -15,11 +14,6 @@ function getDefaultState() {
     duplicateEventsToAdd: {} as Record<string, EventBlock>, // if an event is added, but the liveId already exists, it's added here, and that liveEvent will be canceled , once that event is removed, this event will be added to the end of the chain
     //
     variablesByName: {} as Record<string, any>, // variables that are set in the chain
-  };
-}
-
-const getDefaultRefs = () => ({});
-
-const startStates = {} as InitialItemsState<typeof getDefaultState>;
-
-export const chainsStore = { getDefaultState, getDefaultRefs, startStates };
+  }),
+  newRefs: () => ({}),
+};
